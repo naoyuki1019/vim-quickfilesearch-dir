31d6e3ed027214bae8589ceb600c09552cddfe30
naoyuki onishi <naoyuki.onishi@gmail.com>
2018-08-15 12:10:51 +0900

    fix

--- _.lsfile.bat
+++ _.lsfile.bat
@@ -1,4 +1,4 @@
 rem dir & findstr
 rem dir /s /b /a-d * | findstr /i /v "\\\.git\\" > .lsfile
-dir /s /b /a-d *.php *.tpl *.css *.js *.css  > .lsfile
+dir /s /b /a-d *.php *.tpl *.css *.js  > .lsfile
 
